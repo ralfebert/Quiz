5b26acb55117c3fb57efcc986d847bba3d578a33
Ralf Ebert <info@ralfebert.de>
2019-08-30 21:20:57 +0200

    Project setup Quiz

---
+++ Quiz/App/QuizAppDelegate.swift
@@ -0,0 +1,14 @@
+// © 2019 Ralf Ebert — Quiz
+
+import UIKit
+
+@UIApplicationMain
+class QuizAppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
